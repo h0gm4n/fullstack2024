e82ac895b64c616ef474c1d55726d9b08f5c4820
h0gm4n <atte.hogman@gmail.com>
2024-09-09 14:28:20 +0300

    1.4

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,17 +1,19 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Header = (props) => {
     return (
@@ -23,12 +25,12 @@ const App = () => {
     )
   }
 
-  const Content = (props) => {
+  const Content = () => {
     return (
       <div>
-        <Part part={part1.name} exercise={part1.exercises} />
-        <Part part={part2.name} exercise={part2.exercises} />
-        <Part part={part3.name} exercise={part3.exercises} />
+        <Part part={parts[0].name} exercise={parts[0].exercises} />
+        <Part part={parts[1].name} exercise={parts[1].exercises} />
+        <Part part={parts[2].name} exercise={parts[2].exercises} />
       </div>
     )
   }
@@ -53,7 +55,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content></Content>
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Total exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 }
