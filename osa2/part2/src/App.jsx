c6d8c4f21dfa9dcbc93133346704ff6a3bc3f36c
h0gm4n <atte.hogman@gmail.com>
2024-09-16 19:30:11 +0300

    2.6

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -1,53 +1,56 @@
-const Header = ({ course }) => <h1>{course}</h1>
+import Course from './components/Course'
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const App = ({ course }) => {
 
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    <ul>
-      {parts.map(part => <Part key={part.id} part={part}></Part>)}
-    </ul>
-  </>
-
-const App = () => {
-
-  const Course = () =>
-    <div>
-      <Header course={course.name}></Header>
-      <Content parts={course.parts}></Content>
-      <Total sum={course.parts.map(part => part.exercises).reduce((sum, num) => sum + num)}></Total>
-    </div>
-
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 13,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course => <Course key={course.id} course={course}></Course>)}
     </div>
   )
 }
---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    event.preventDefault()
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        <button onClick={addPerson} type="submit">add</button>
+      </div>
+      <h2>Numbers</h2>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+    </div>
+  )
+
+}
+
+export default App
