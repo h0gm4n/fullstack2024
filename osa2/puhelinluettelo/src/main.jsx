ee9bb392c7b18c01de661b8bddc5250d7b668f83
h0gm4n <atte.hogman@gmail.com>
2024-09-17 15:35:51 +0300

    2.11

--- osa2/puhelinluettelo/src/main.jsx
+++ osa2/puhelinluettelo/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import App from './App.jsx'
 import './index.css'
 
@@ -8,3 +9,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+const promise = axios.get('http://localhost:2001/')
