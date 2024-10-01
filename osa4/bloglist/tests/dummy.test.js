05840a63f81791ac86aebbf17d99a9c7cb3b962a
h0gm4n <atte.hogman@gmail.com>
2024-10-01 19:28:22 +0300

    4.5

--- osa4/bloglist/tests/dummy.test.js
+++ osa4/bloglist/tests/dummy.test.js
@@ -8,3 +8,51 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+            title: 'Kauppakassi blog',
+            author: 'Pasi Viheraho',
+            url: 'pasiviheraho.com',
+            likes: 40,
+            id: '66fc1c6e7157f7a558d4ee51',
+        }
+    ]
+
+    test('when list has only one blog equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 40)
+    })
+})
+
+describe('favorite blog', () => {
+    const listWithThreeBlogs = [
+        {
+            title: 'title1',
+            author: 'author1',
+            url: 'url1',
+            likes: 37,
+            id: 'id1',
+        },
+        {
+            title: 'title2',
+            author: 'author2',
+            url: 'url2',
+            likes: 46,
+            id: 'id2',
+        },
+        {
+            title: 'title3',
+            author: 'author3',
+            url: 'url3',
+            likes: 33,
+            id: 'id3',
+        }
+    ]
+    const biggest = listWithThreeBlogs[1]
+    test('equals the most liked blog', () => {
+        const result = listHelper.favoriteBlog(listWithThreeBlogs)
+        assert.strictEqual(result, biggest)
+    })
+})
--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -1,7 +1,19 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((accumulator, current) => accumulator + current.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    const blogsByLikes = blogs.sort((a, b) => b.likes - a.likes)
+    const blogsByLikesFirst = blogsByLikes[0]
+    return blogsByLikesFirst
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes, favoriteBlog
 }
