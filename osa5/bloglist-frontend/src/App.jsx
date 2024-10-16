304efdb95202313dac1eec7eb960f2a83f74f3bd
h0gm4n <atte.hogman@gmail.com>
2024-10-16 17:33:13 +0300

    5.4

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -4,11 +4,38 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import './index.css'
 
+const CreateNew = ({ title, author, url, setTitle, setAuthor, setUrl, addNewBlog }) => {
+  return (
+    <form onSubmit={addNewBlog}>
+      <div>
+        <h1>Create new</h1>
+        <div>
+          title:
+          <input type="text" value={title} name="Title" onChange={({ target }) => setTitle(target.value)}></input>
+        </div>
+        <div>
+          author:
+          <input type="text" value={author} name="Author" onChange={({ target }) => setAuthor(target.value)}></input>
+        </div>
+        <div>
+          url:
+          <input type="text" value={url} name="Url" onChange={({ target }) => setUrl(target.value)}></input>
+        </div>
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+}
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -53,6 +80,24 @@ const App = () => {
     console.log('logging in with', username, password)
   }
 
+  const addNewBlog = async (event) => {
+    event.preventDefault()
+    let newBlog = {
+      title: title,
+      author: author,
+      url: url
+    }
+    blogService.create(newBlog)
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+    setBlogs(blogs.concat(newBlog))
+    setSuccessMessage(`a new blog ${title} by ${author} added`)
+    setTimeout(() => {
+      setSuccessMessage(null)
+    }, 5000)
+  }
+
   const handleLogout = async (event) => {
     event.preventDefault()
     window.localStorage.removeItem('loggedBlogappUser')
@@ -62,9 +107,11 @@ const App = () => {
   if (user !== null) {
     return (
       <div>
+        <div className='success'>{successMessage}</div>
+        <div className='error'>{errorMessage}</div>
         Logged in as <b>{user.username}</b>
         <button onClick={handleLogout}>logout</button>
-
+        <CreateNew title={title} author={author} url={url} setTitle={setTitle} setAuthor={setAuthor} setUrl={setUrl} addNewBlog={addNewBlog} ></CreateNew>
         <h2>blogs</h2>
         {
           blogs.map(blog =>
