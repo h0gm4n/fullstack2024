c1ef405968b9e1ec13025cb0846f3c115b910e9c
h0gm4n <atte.hogman@gmail.com>
2024-10-17 14:38:27 +0300

    5.6

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -3,30 +3,9 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
+import CreateNew from './components/CreateNew'
 import './index.css'
 
-const CreateNew = ({ title, author, url, setTitle, setAuthor, setUrl, addNewBlog }) => {
-  return (
-    <form onSubmit={addNewBlog}>
-      <div>
-        <h1>Create new</h1>
-        <div>
-          title:
-          <input type="text" value={title} name="Title" onChange={({ target }) => setTitle(target.value)}></input>
-        </div>
-        <div>
-          author:
-          <input type="text" value={author} name="Author" onChange={({ target }) => setAuthor(target.value)}></input>
-        </div>
-        <div>
-          url:
-          <input type="text" value={url} name="Url" onChange={({ target }) => setUrl(target.value)}></input>
-        </div>
-      </div>
-      <button type="submit">create</button>
-    </form>
-  )
-}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
